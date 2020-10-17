3d12e14da7a7d538047bb8fd97b8068472f3d376
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-10-17 21:52:35 +0300

    Add spaces in header formatting.

--- src/components/Case/Header.tsx
+++ src/components/Case/Header.tsx
@@ -5,9 +5,9 @@ import {ICase} from "./Case";
 export default function Header({'case': theCase}: { case: ICase }) {
     return (
         <Card.Header>
-            <strong>#1</strong>
-            {theCase.platform} |
-            {theCase.language} |
+            <strong>#1</strong>&nbsp;
+            {theCase.platform} |&nbsp;
+            {theCase.language} |&nbsp;
             {theCase.commander_name}
         </Card.Header>
     );
