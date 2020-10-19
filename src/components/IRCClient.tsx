0bea848104a90eece68a07cb60dd5ee808fc0be2
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-10-19 18:14:41 +0300

    Add irc-ws gateway.

--- src/components/IRCClient.tsx
+++ src/components/IRCClient.tsx
@@ -14,17 +14,15 @@ import IRC from 'irc-framework';
 interface IIRCClientCredentials {
     host: string
     port: number
-    nickName: string
-    password: string
-    realName: string
+    path: string
+    nick: string
 }
 
 const CREDENTIALS: IIRCClientCredentials = {
-    host: 'irc.fuelrats.com',
-    port: 6697,
-    nickName: 'FuRBoard',
-    password: '',
-    realName: 'FuRBoard',
+    host: 'localhost',
+    port: 8010,
+    path: '/webirc/websocket/',
+    nick: 'DiRaven[FuRBoard]',
 }
 
 interface IIRCClientProps {
@@ -34,229 +32,226 @@ interface IIRCClientProps {
 export default function IRCClient(props: IIRCClientProps) {
     const {dispatch} = props
 
-//     useEffect(() => {
-//         let client = new IRC.Client();
-//         client.connect({
-//             host: 'kiwi.fuelrats.com',
-//             port: '4443',
-//             nick: CREDENTIALS.nickName,
-//             path: '/webirc/kiwiirc/795/3wokhgcz/websocket',
-//             direct: true,
-//             tls: true,
-//         });
-//         // https://kiwi.fuelrats.com:4443/webirc/kiwiirc/"
-//         client.on('connected', function(event: any) {
-//             console.log('connected')
-//             client.join('#ratchat');
-//         })
-//
-//         client.on('message', function(event: any) {
-//             // if (event.message.indexOf('hello') === 0) {
-//             //     event.reply('Hi!');
-//             // }
-//
-//             if (event.message.match(/^!join /)) {
-//                 // let to_join = event.message.split(' ');
-//                 // event.reply('Joining ' + to_join + '..');
-//                 // client.join('#ratchat');
-//             }
-//         });
-//
-//
-// // Or a quicker to match messages...
+    useEffect(() => {
+        let client = new IRC.Client();
+        client.connect(CREDENTIALS);
+        // https://kiwi.fuelrats.com:4443/webirc/kiwiirc/"
+        client.on('connected', function (event: any) {
+            console.log('connected')
+            client.join('#ratchat');
+        })
+
+        client.on('message', function (event: any) {
+            console.log(event)
+            // if (event.message.indexOf('hello') === 0) {
+            //     event.reply('Hi!');
+            // }
+
+            if (event.message.match(/^!join /)) {
+                // let to_join = event.message.split(' ');
+                // event.reply('Joining ' + to_join + '..');
+                // client.join('#ratchat');
+            }
+        });
+    })
+
+    return (<></>);
+}
+
+
+// Or a quicker to match messages...
 //         client.matchMessage(/^hi/, function(event: any) {
 //             event.reply('hello there!');
 //         });
 
-        // console.log('connect')
-        // const stream = createIrcStream(CREDENTIALS)
-        //
-        // // dispatch({
-        // //     type: 'REQUEST_CONNECTION_PENDING',
-        // //     connection: {
-        // //         id,
-        // //         isConnected: false,
-        // //         isWelcome: false,
-        // //         nickname,
-        // //         realName,
-        // //         server,
-        // //         port,
-        // //         stream,
-        // //         error: null,
-        // //         conversations: null
-        // //     }
-        // // })
-        //
-        // stream.on('errors', (e: any) => {
-        //     console.log(e)
-        //     // dispatch({type: 'IRC_ERROR', connectionId: id, message: e.message})
-        // })
-        //
-        // stream.on('mode', (e) => {
-        //     console.log('mode', e)
-        // })
-        //
-        // stream.on('invite', (e: any) => {
-        //     console.log('invite', e)
-        // })
-        //
-        // stream.on('notice', (e) => {
-        //     console.log('notice', e)
-        //
-        //     // const channelInMessage = getChannelFromNotice(e.message)
-        //     //
-        //     // var to, message
-        //     // if (channelInMessage) {
-        //     //     const splitMessage = e.message.split(' ')
-        //     //     splitMessage.shift() // remove leading channel name
-        //     //
-        //     //     to = channelInMessage
-        //     //     message = splitMessage.join(' ')
-        //     // } else {
-        //     //     to = e.to
-        //     //     message = e.message
-        //     // }
-        //     //
-        //     // dispatch({
-        //     //     type: 'RECEIVE_NOTICE',
-        //     //     connectionId: id,
-        //     //     from: e.from,
-        //     //     to,
-        //     //     message
-        //     // })
-        // })
-        //
-        // stream.on('away', (e) => {
-        //     console.log('away', e)
-        //     // dispatch({
-        //     //     type: 'RECEIVE_AWAY',
-        //     //     connectionId: id,
-        //     //     nick: e.nick,
-        //     //     message: e.message
-        //     // })
-        // })
-        //
-        // stream.on('part', (e) => {
-        //     console.log('part', e)
-        //     // dispatch({
-        //     //     type: 'RECEIVE_PART',
-        //     //     connectionId: id,
-        //     //     nick: e.nick,
-        //     //     message: e.message,
-        //     //     channels: e.channels
-        //     // })
-        // })
-        //
-        // stream.on('quit', (e) => {
-        //     console.log('quit', e)
-        //     // dispatch({
-        //     //     type: 'RECEIVE_QUIT',
-        //     //     connectionId: id,
-        //     //     nick: e.nick,
-        //     //     message: e.message
-        //     // })
-        // })
-        //
-        // stream.on('kick', (e: any) => {
-        //     console.log('kick', e)
-        // })
-        //
-        // stream.on('motd', (e) => {
-        //     console.log('motd', e)
-        //     // dispatch({
-        //     //     type: 'RECEIVE_MOTD',
-        //     //     connectionId: id,
-        //     //     motd: e.motd.join('\n')
-        //     // })
-        // })
-        //
-        // stream.on('welcome', (e) => {
-        //     console.log('welcome', e)
-        //     //
-        //     // dispatch({
-        //     //     type: 'RECEIVE_WELCOME',
-        //     //     connectionId: id,
-        //     //     nick
-        //     // })
-        // })
-        //
-        // stream.on('nick', (e) => {
-        //     console.log('nick', e)
-        //     // dispatch({
-        //     //     type: 'RECEIVE_NICK',
-        //     //     connectionId: id,
-        //     //     oldNickname: e.nick,
-        //     //     newNickname: e.new
-        //     // })
-        // })
-        //
-        // stream.on('topic', (e) => {
-        //     console.log('topic', e)
-        //     // dispatch({
-        //     //     type: 'RECEIVE_TOPIC',
-        //     //     connectionId: id,
-        //     //     channel: e.channel,
-        //     //     topic: e.topic
-        //     // })
-        // })
-        //
-        // stream.on('join', (e) => {
-        //     console.log('join', e)
-        //     // dispatch({
-        //     //     type: 'RECEIVE_JOIN',
-        //     //     connectionId: id,
-        //     //     channel: e.channel,
-        //     //     from: e.nick
-        //     // })
-        // })
-        //
-        // stream.on('names', (e: any) => {
-        //     console.log('names', e)
-        //     // dispatch({
-        //     //     type: 'RECEIVE_NAMES',
-        //     //     connectionId: id,
-        //     //     channel: e.channel,
-        //     //     names: e.names
-        //     // })
-        // })
-        //
-        // stream.on('message', (e) => {
-        //     if (e.message.trim().startsWith('\u0001ACTION')) {
-        //         console.log('action', e)
-        //         // dispatch({
-        //         //     type: 'RECEIVE_ACTION',
-        //         //     connectionId: id,
-        //         //     channel: equalNames(e.to, nickname) ? e.from : e.to,
-        //         //     from: e.from,
-        //         //     message: `${e.from} ${e.message
-        //         //         /* eslint-disable no-control-regex */
-        //         //         .replace(/^\u0001ACTION /, '')
-        //         //         /* eslint-disable no-control-regex */
-        //         //         .replace(/\u0001$/, '')}`
-        //         // })
-        //     } else if (e.to.toLowerCase() === CREDENTIALS.nickName.toLowerCase()) {
-        //         console.log('direct msg', e)
-        //         // dispatch({
-        //         //     type: 'RECEIVE_DIRECT_MESSAGE',
-        //         //     connectionId: id,
-        //         //     from: e.from,
-        //         //     message: e.message
-        //         // })
-        //     } else {
-        //         console.log('channel msg', e)
-        //         // dispatch({
-        //         //     type: 'RECEIVE_CHANNEL_MESSAGE',
-        //         //     connectionId: id,
-        //         //     channel: e.to,
-        //         //     from: e.from,
-        //         //     message: e.message
-        //         // })
-        //     }
-        // })
-    // }, [])
+// console.log('connect')
+// const stream = createIrcStream(CREDENTIALS)
+//
+// // dispatch({
+// //     type: 'REQUEST_CONNECTION_PENDING',
+// //     connection: {
+// //         id,
+// //         isConnected: false,
+// //         isWelcome: false,
+// //         nickname,
+// //         realName,
+// //         server,
+// //         port,
+// //         stream,
+// //         error: null,
+// //         conversations: null
+// //     }
+// // })
+//
+// stream.on('errors', (e: any) => {
+//     console.log(e)
+//     // dispatch({type: 'IRC_ERROR', connectionId: id, message: e.message})
+// })
+//
+// stream.on('mode', (e) => {
+//     console.log('mode', e)
+// })
+//
+// stream.on('invite', (e: any) => {
+//     console.log('invite', e)
+// })
+//
+// stream.on('notice', (e) => {
+//     console.log('notice', e)
+//
+//     // const channelInMessage = getChannelFromNotice(e.message)
+//     //
+//     // var to, message
+//     // if (channelInMessage) {
+//     //     const splitMessage = e.message.split(' ')
+//     //     splitMessage.shift() // remove leading channel name
+//     //
+//     //     to = channelInMessage
+//     //     message = splitMessage.join(' ')
+//     // } else {
+//     //     to = e.to
+//     //     message = e.message
+//     // }
+//     //
+//     // dispatch({
+//     //     type: 'RECEIVE_NOTICE',
+//     //     connectionId: id,
+//     //     from: e.from,
+//     //     to,
+//     //     message
+//     // })
+// })
+//
+// stream.on('away', (e) => {
+//     console.log('away', e)
+//     // dispatch({
+//     //     type: 'RECEIVE_AWAY',
+//     //     connectionId: id,
+//     //     nick: e.nick,
+//     //     message: e.message
+//     // })
+// })
+//
+// stream.on('part', (e) => {
+//     console.log('part', e)
+//     // dispatch({
+//     //     type: 'RECEIVE_PART',
+//     //     connectionId: id,
+//     //     nick: e.nick,
+//     //     message: e.message,
+//     //     channels: e.channels
+//     // })
+// })
+//
+// stream.on('quit', (e) => {
+//     console.log('quit', e)
+//     // dispatch({
+//     //     type: 'RECEIVE_QUIT',
+//     //     connectionId: id,
+//     //     nick: e.nick,
+//     //     message: e.message
+//     // })
+// })
+//
+// stream.on('kick', (e: any) => {
+//     console.log('kick', e)
+// })
+//
+// stream.on('motd', (e) => {
+//     console.log('motd', e)
+//     // dispatch({
+//     //     type: 'RECEIVE_MOTD',
+//     //     connectionId: id,
+//     //     motd: e.motd.join('\n')
+//     // })
+// })
+//
+// stream.on('welcome', (e) => {
+//     console.log('welcome', e)
+//     //
+//     // dispatch({
+//     //     type: 'RECEIVE_WELCOME',
+//     //     connectionId: id,
+//     //     nick
+//     // })
+// })
+//
+// stream.on('nick', (e) => {
+//     console.log('nick', e)
+//     // dispatch({
+//     //     type: 'RECEIVE_NICK',
+//     //     connectionId: id,
+//     //     oldNickname: e.nick,
+//     //     newNickname: e.new
+//     // })
+// })
+//
+// stream.on('topic', (e) => {
+//     console.log('topic', e)
+//     // dispatch({
+//     //     type: 'RECEIVE_TOPIC',
+//     //     connectionId: id,
+//     //     channel: e.channel,
+//     //     topic: e.topic
+//     // })
+// })
+//
+// stream.on('join', (e) => {
+//     console.log('join', e)
+//     // dispatch({
+//     //     type: 'RECEIVE_JOIN',
+//     //     connectionId: id,
+//     //     channel: e.channel,
+//     //     from: e.nick
+//     // })
+// })
+//
+// stream.on('names', (e: any) => {
+//     console.log('names', e)
+//     // dispatch({
+//     //     type: 'RECEIVE_NAMES',
+//     //     connectionId: id,
+//     //     channel: e.channel,
+//     //     names: e.names
+//     // })
+// })
+//
+// stream.on('message', (e) => {
+//     if (e.message.trim().startsWith('\u0001ACTION')) {
+//         console.log('action', e)
+//         // dispatch({
+//         //     type: 'RECEIVE_ACTION',
+//         //     connectionId: id,
+//         //     channel: equalNames(e.to, nickname) ? e.from : e.to,
+//         //     from: e.from,
+//         //     message: `${e.from} ${e.message
+//         //         /* eslint-disable no-control-regex */
+//         //         .replace(/^\u0001ACTION /, '')
+//         //         /* eslint-disable no-control-regex */
+//         //         .replace(/\u0001$/, '')}`
+//         // })
+//     } else if (e.to.toLowerCase() === CREDENTIALS.nickName.toLowerCase()) {
+//         console.log('direct msg', e)
+//         // dispatch({
+//         //     type: 'RECEIVE_DIRECT_MESSAGE',
+//         //     connectionId: id,
+//         //     from: e.from,
+//         //     message: e.message
+//         // })
+//     } else {
+//         console.log('channel msg', e)
+//         // dispatch({
+//         //     type: 'RECEIVE_CHANNEL_MESSAGE',
+//         //     connectionId: id,
+//         //     channel: e.to,
+//         //     from: e.from,
+//         //     message: e.message
+//         // })
+//     }
+// })
+// }, [])
 
-    // return (<></>);
-}
+// }
 
 
 // import net from 'net'
