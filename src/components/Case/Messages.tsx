dba9ea680bad714199fde58cd0a45d6e3a6548f6
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-10-20 15:16:49 +0300

    Add simplistic messages formatting.

--- src/components/Case/Messages.tsx
+++ src/components/Case/Messages.tsx
@@ -1,8 +1,24 @@
 import React, {useEffect, useRef} from 'react';
 import Table from 'react-bootstrap/Table';
 
+
 interface IMessagesProps {
     messages: string[]
+}
+
+const formatters = [
+    [/\d+j/gi, '<strong class="text-info">$&</strong>'],
+    [/^[^:]+:/gi, '<strong class="text-info">$&</strong>'],
+    [/#\d+/gi, '<strong class="text-info">$&</strong>'],
+    [
+        /stdn|fr[+-]?|wr[+-]?|bc[+1]/gi,
+        '<strong class="text-info">$&</strong>',
+    ],
+]
+function formatMessage(m: string): string {
+    // @ts-ignore
+    formatters.forEach(formatter => m = m.replace(formatter[0], formatter[1]))
+    return m
 }
 
 export default function Messages(props: IMessagesProps) {
@@ -20,14 +36,16 @@ export default function Messages(props: IMessagesProps) {
     useEffect(scrollToBottom, [messages]);
 
     return (
-        <div ref={scrollableRef}  className='overflow-auto border border-primary messages h-100'>
+        <div ref={scrollableRef} className='overflow-auto border border-primary messages h-100'>
             <Table striped borderless hover size='sm' className='m-0'>
                 <tbody>
                     {
                         (
                             messages.length > 0 &&
-                            React.Children.map(messages, (n) => <tr>
-                                <td>{n}</td>
+                            React.Children.map(messages, (m) => <tr>
+                                <td dangerouslySetInnerHTML={{
+                                    __html: formatMessage(m)
+                                }}/>
                             </tr>)
                         ) ||
                         <tr>
